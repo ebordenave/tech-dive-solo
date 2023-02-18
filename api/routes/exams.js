35458b36420abdec3c2e397a9db0b66a18b4450b
Edward Bordenave <edward.bordenave@gmail.com>
2023-02-17 20:21:22 -0500

    initial commit

---
+++ api/routes/exams.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+
+/* GET all exams */
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all exams'});
+});
+
+/* GET a single exam */
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET a single exam'});
+});
+
+/* POST a new exam */
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new exam'});
+});
+
+/* DELETE an exam */
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE an exam'});
+});
+
+/* UPDATE a exam */
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE an exam'});
+});
+
+module.exports = router;
