697e8173b3eee682efd1d358173263a87f2f26a7
Edward Bordenave <edward.bordenave@gmail.com>
2023-02-17 22:15:01 -0500

    added exams model and patient route js files.

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
 const examRoutes = require('./routes/exams')
 
 //express app
