397dda893d4617e65bc96174fb8fa13a94e66956
Edward Bordenave <edward.bordenave@gmail.com>
2023-03-07 10:25:02 -0500

    added AppBar and installed react-router package

---
+++ client/src/components/ExamsTable.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState, useMemo } from 'react';
+import MaterialReactTable from 'material-react-table';
+import { Link } from 'react-router-dom';
+
+function ExamsTable() {
+  const [data, setExamData] = useState([]);
+  const url = 'https://czi-covid-1-hjgxknco3a-uc.a.run.app/api/exams';
+
+  async function fetchData(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data.exams);
+      setExamData(data.exams);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(url);
+  }, []);
+
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'patientId',
+        header: 'Patient ID',
+        width: 20
+      },
+      {
+        accessorKey: 'examId',
+        header: 'Exam ID',
+        width: 20,
+        Cell: ({ cell: { value }, row: { original } }) => <Link to={`users/${original.id}`}>{value}</Link>
+      },
+      {
+        accessorKey: 'imageURL',
+        header: 'Image',
+        width: 20,
+        render : rowData => (
+          <img
+            src={rowData.imageUrl}
+            style={{ width: 50, height: 50 }}
+          />
+        )
+      },
+      {
+        accessorKey: 'keyFindings',
+        header: 'Key Findings',
+        width: 20
+      },
+      {
+        accessorKey: 'brixiaScores',
+        header: 'Brixia Scores',
+        width: 20
+      },
+      {
+        accessorKey: 'age',
+        header: 'Age',
+        width: 20
+      },
+      {
+        accessorKey: 'sex',
+        header: 'Sex',
+        width: 20
+      },
+      {
+        accessorKey: 'bmi',
+        header: 'BMI',
+        width: 20
+      },
+      {
+        accessorKey: 'zipCode',
+        header: 'Zip Code',
+        width: 20
+      },
+    ],
+    [],
+  );
+
+  return (
+    <div style={{ maxWidth: '75%', margin: 'auto'}}>
+      <h1>Exam Data</h1>
+      <MaterialReactTable columns={columns} data={data} />
+    </div>
+  );
+}
+
+export default ExamsTable;
